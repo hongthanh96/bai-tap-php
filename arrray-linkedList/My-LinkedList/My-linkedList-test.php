74cca4f5db13ef1f71ad50ea974706340a09a799
hongthanh96 <hongthanh010896@gmail.com>
2020-06-13 19:58:06 +0700

    dfgh

---
+++ arrray-linkedList/My-LinkedList/My-linkedList-test.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once ('My-linkedList.php');
+
+$linkedList = new LinkList();
+
+$linkedList->insertFirst(11);
+$linkedList->insertFirst(22);
+$linkedList->insertLast(33);
+$linkedList->insertLast(44);
+$linkedList->insertLast(43);
+$linkedList->insertLast(21);
+$linkedList->deleteNode(43);
+echo $linkedList->totalNodes();
+?>
