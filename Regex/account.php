45e94d81952850613225aae8fb8589ae603ee91f
hongthanh96 <hongthanh010896@gmail.com>
2020-06-19 15:18:21 +0700

    ắedrftg

--- Regex/account.php
+++ Regex/account.php
@@ -1,28 +1,32 @@
 <?php
-    function checkAccount($str){
-        $pattern = '/^[_a-z0-9]{6,}$/';
-        if(preg_match($pattern,$str)){
-            echo "Email hợp lệ!";
-        }
-        else{
-            echo "Email không hợp lệ!";
-        }
+function checkAccount($str)
+{
+    $pattern = '/^[_a-z0-9]{6,}$/';
+    if (preg_match($pattern, $str)) {
+        echo "Email hợp lệ!";
+    } else {
+        echo "Email không hợp lệ!";
     }
-    if($_SERVER["REQUEST_METHOD"] === "POST"){
-        $str = $_POST["str"];
-        checkAccount($str);
-    }
+}
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $str = $_POST["str"];
+    checkAccount($str);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Validate account</title>
 </head>
 <body>
-    <label for="">Nhâp chuỗi string: </label>
-    <input type="text" name="str" id="">
-    <input type="submit" value="submit">
+    <form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
+        <label for="">Nhâp chuỗi string: </label>
+        <input type="text" name="str" id="">
+        <input type="submit" value="submit">
+    </form>
 </body>
+
 </html>
